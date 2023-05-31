7ec3ab81c1f3adc3b06e281e762411c274fa7790
Jodokus Heck <jodokus.heck@gmail.com>
2023-05-31 21:37:01 +0200

    updated demo.py

--- demo.py
+++ demo.py
@@ -8,7 +8,5 @@ import requests, json
 # get rat cookie from firefox in this case
 rat = requests.utils.dict_from_cookiejar(browsercookie.firefox()).get('rat')
 
-session = requests.Session()
-
 pirate = APIrate.Pirate(rat)
 print(pirate.get_profilev2("balance").get("gold"))
